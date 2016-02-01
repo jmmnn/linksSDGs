6f1d6216aeb0722295596acc3746a40217b54d2b
jmmnn_1 <jmmnn@yahoo.com>
2016-02-01 03:35:51 +0000

    Webapp v_2_4

---
+++ Webapp_v_2_4_landmark_wkg_SUBMISSION_2.4/dependencies.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# Setting up the environment
+
+import subprocess
+import sys
+
+#List commands to execute here.
+
+SOLRPY = "sudo pip install -U solrpy"
+
+
+#order commands in sequence
+cmds = [SOLRPY]
+
+#Iterates over list, running statements for each item in the list
+count=0
+for cmd in cmds:
+    count+=1
+    print "Running Command Number %s" % count
+    subprocess.call(cmd, shell=True)
---
+++ Webapp_v_2_4_landmark_wkg_SUBMISSION_2.4/readme.md
@@ -0,0 +1,9 @@
+This is how you run the web app:
+
+1) Install required packages in your machine, run:
+$ python dependencies.py
+
+2) To start the web app run:
+$python index.py
+
+3) To update the Chord diagramm open the data_chords.py in an editor to read instructions.
