1aaa704efa9f7c8962ed9702dab36f2edc842dbe
jmmnn_1 <jmmnn@yahoo.com>
2016-02-24 04:52:33 +0000

    added firewall status to security script

--- security_setup.py
+++ security_setup.py
@@ -12,9 +12,10 @@ OPEN_WEBAPP = "sudo ufw allow 8080"
 OPEN_SOLR = "sudo ufw allow 8081"   #use during development or to test access to Solr server
 ENABLE_FIREWALL = "sudo ufw enable"
 DISABLE_FIREWALL = "sudo ufw disable" #use to stop the firewall and alow all traffic
+FIREWALL_STATUS = "sudo ufw status" #use to view the firewall status
 
 #order commands in sequence
-cmds = [OPEN_SSH, OPEN_WEBAPP, ENABLE_FIREWALL]
+cmds = [OPEN_SSH, OPEN_WEBAPP, ENABLE_FIREWALL, FIREWALL_STATUS]
 
 #Iterates over list, running statements for each item in the list
 count=0
